f6b1408386e80012a9201bafd7f14b7983d93a2d
Cyclopsie <eyepurplecat@gmail.com>
2023-02-20 21:23:59 -0800

    addEventListeners to buttons

--- cal.js
+++ cal.js
@@ -14,5 +14,46 @@ function multiply(a, b) {
 }
 
 function operate(a, b, operator) {
-    return operator(a, b);
+    let answer = operator(a, b);
+    return document.querySelector('.display').textContent = answer;
+    
 }
+
+let buttonStorage = [];
+let buttPressed = ""
+
+let buttAll = document.querySelectorAll('.num');
+buttAll.forEach((button) => {
+    button.addEventListener('click', () => {
+        buttPressed = button.id;
+        
+        buttonStorage.push(button.id);
+        console.log(buttonStorage);
+        return document.querySelector('.display').textContent = buttPressed;
+    }
+    )
+});
+
+let operatorChoice = "";
+let operAll = document.querySelectorAll('.operator');
+operAll.forEach((button) => {
+    button.addEventListener('click', () => {
+        operatorChoice = button.id
+        return document.querySelector('.display').textContent = operatorChoice;
+    }
+    )
+});
+
+
+
+let equalButt = '';
+let equalAll = document.querySelectorAll('.equalsign');
+equalAll.forEach((button) => {
+    button.addEventListener('click', () => {
+        c = buttonStorage[0];
+        d = buttonStorage[1];
+        e = operatorChoice
+        return operate(c, d, e);
+    }
+    )
+});
