4133ff91078bc9161398bf490a5381642e1c4b93
Cyclopsie <eyepurplecat@gmail.com>
2023-02-26 01:25:56 -0800

    First functional build

--- cal.js
+++ cal.js
@@ -1,4 +1,4 @@
-function add(a,b){
+function add(a, b){
     return a + b;
 }
 function subtract(a, b) {
@@ -14,64 +14,134 @@ function multiply(a, b) {
 }
 
 function operate(a, b, operator) {
-    let answer = operator(a, b);
-    return document.querySelector('.display').textContent = answer;
+    return operator(a, b);
+    
     
 }
 
+/////////number input
+
 let buttonStorage = [];
-let buttPressed = ""
+let buttonTogether = ""
 
 let buttAll = document.querySelectorAll('.num');
 buttAll.forEach((button) => {
     button.addEventListener('click', () => {
-        buttPressed = button.id;
-        
-        buttonStorage.push(button.id);
-        console.log(buttonStorage);
-        return document.querySelector('.display').textContent = buttPressed;
-    }
-    )
+       //I need to catetcon each of the buttons pressed, but also store them together until an operator is chosen
+       buttonStorage.push(button.id);
+            for(i = 0; i <= buttonStorage.length-1; i++) {
+                if(i === 0) {
+                    buttonTogether = buttonStorage[0];
+                    document.querySelector('.display').textContent = buttonTogether;
+                } else if (i >= 1) {
+                    buttonTogether = buttonStorage.join('');
+                    document.querySelector('.display').textContent = buttonTogether;
+                }
+            }
+    })
 });
 
-let operatorChoice = "";
+
+
+let operatorChoice = '';
 let operAll = document.querySelectorAll('.operator');
 operAll.forEach((button) => {
     button.addEventListener('click', () => {
         if(button.id === 'multiply') {
-            return document.querySelector('.display').textContent = "*";
+            document.querySelector('.display').textContent = "*";
+            //operator = multiply;
+            a = +buttonTogether;
+            buttonTogether = "";
+            buttonStorage = [];
+            
         } else if (button.id === 'subtract') {
-            return document.querySelector('.display').textContent = "-";
+            document.querySelector('.display').textContent = "-";
+            a = +buttonTogether;
+            buttonTogether = "";
+            buttonStorage = [];
         } else if (button.id === 'divide') {
-            return document.querySelector('.display').textContent = "/";
+            document.querySelector('.display').textContent = "/";
+            a = +buttonTogether;
+            buttonTogether = "";
+            buttonStorage = [];
+
+        } else if (button.id === 'add') {
+            document.querySelector('.display').textContent = "+";
+            a = +buttonTogether;
+            buttonTogether = "";
+            buttonStorage = [];
         } else {
-            return document.querySelector('.display').textContent = "+";
+            console.log(operatorChoice)
         }
-        operatorChoice = button.id
-        return document.querySelector('.display').textContent = operatorChoice;
+       operatorChoice = button.id
+       
+       
     }
     )
 });
 
 
-
+let d = '';
 let equalButt = '';
 let equalAll = document.querySelectorAll('.equalsign');
 equalAll.forEach((button) => {
     button.addEventListener('click', () => {
         if (operatorChoice === 'multiply') {
-            let mul = operate(buttonStorage[0], buttonStorage[1], multiply);
-            return document.querySelector('.display').textContent = mul;
+         //  operator = operatorChoice;
+            b = +buttonTogether;
+            //operator = ;
+            document.querySelector('.display').textContent = operate(a, b, multiply);
         } else if (operatorChoice === 'subtract') {
-            let sub = operate(buttonStorage[0], buttonStorage[1], subtract);
-            return document.querySelector('.display').textContent = sub;
+            b = +buttonTogether;
+            document.querySelector('.display').textContent = operate(a, b, subtract);
         } else if (operatorChoice === 'divide') {
-            let divi = operate(buttonStorage[0], buttonStorage[1], divide);
-            return document.querySelector('.display').textContent = divi;
-        } else {
-            let addition = operate(buttonStorage[0], buttonStorage[1], add);
-            return document.querySelector('.display').textContent = addition;
+            b = +buttonTogether;
+            document.querySelector('.display').textContent = operate(a, b, divide);
+        } else if (operatorChoice === 'add') {
+            b = +buttonTogether;
+            document.querySelector('.display').textContent = operate(a, b, add);
         }
-    }
-    )
+        
+    })
+    
 });
+
+
+
+
+let clearLog = document.querySelector('.clear');
+clearLog.forEach((button) => {
+    button.addEventListener('click', () => {
+        document.location.reload(true);
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+-store the number input into a variable
+-when an operator is clicked
+    -store the number variable
+    -store the operator
+-clear the number variable
+-store new variable
+-when equal sign is clicked
+    -perform the operation
+        -store new variable
+    -clear the variables
+    or - if another operator is clicked
+            -store second variable
+            -perform operation and store as new variable
+            */
